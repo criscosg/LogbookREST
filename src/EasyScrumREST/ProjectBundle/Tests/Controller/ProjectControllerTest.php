aeb800d81da9400fcc7019c45e61989242599cf8
criscosg <criscosg@gmail.com>
2015-01-29 23:12:32 +0100

    test for sprint and test

--- src/EasyScrumREST/ProjectBundle/Tests/Controller/ProjectControllerTest.php
+++ src/EasyScrumREST/ProjectBundle/Tests/Controller/ProjectControllerTest.php
@@ -95,7 +95,6 @@ class ProjectControllerTest extends CustomTestCase
         ->form(array('backlog[title]' => 'nueva tarea',
                 'backlog[description]' => 'Esta es la nueva tarea', 'backlog[priority]'=>10 ));
         $crawler = $this->client->submit($form);
-        $crawler = $this->client->followRedirect();
         $text = utf8_encode("nueva tarea");
         $this->assertGreaterThan(0, $crawler->filter('html:contains("' . $text . '")')->count());
     }
@@ -117,5 +116,44 @@ class ProjectControllerTest extends CustomTestCase
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
         $text = utf8_encode("Tarea backlog test");
         $this->assertEquals(0, $crawler->filter('html:contains("' . $text . '")')->count());
+    }
+    
+    public function testNewIssue()
+    {
+        $crawler = $this->login();
+        $crawler = $this->client->request('GET', 'project/issue/create/1');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+    
+        $form = $crawler->selectButton('Save')
+        ->form(array('issue[title]' => 'nueva incidencia',
+                'issue[description]' => 'Esta es la nueva incidencia', 'issue[priority]'=>10 ));
+        $crawler = $this->client->submit($form);
+        $crawler = $this->client->followRedirect();
+        $text = utf8_encode("nueva incidencia");
+        $this->assertGreaterThan(0, $crawler->filter('html:contains("' . $text . '")')->count());
+    }
+    
+    public function testEditIssue()
+    {
+        $crawler = $this->login();
+        $crawler = $this->client->request('GET', 'project/issue/edit/1');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+    
+        $form = $crawler->selectButton('Save')
+        ->form(array('issue[title]' => 'nueva incidencia modificada',
+                'issue[description]' => 'Esta es la nueva incidencia', 'issue[priority]'=>10 ));
+        $crawler = $this->client->submit($form);
+        $crawler = $this->client->followRedirect();
+        $text = utf8_encode('nueva incidencia modificada');
+        $this->assertGreaterThan(0, $crawler->filter('html:contains("' . $text . '")')->count());
+    }
+    
+    public function testFinalizeIssue()
+    {
+        $crawler = $this->login();
+        $crawler = $this->client->request('GET', 'project/issue/finalize/1');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+        $text = utf8_encode("Tarea issue test");
+        $this->assertGreaterThan(0, $crawler->filter('html:contains("' . $text . '")')->count());
     }
 }
